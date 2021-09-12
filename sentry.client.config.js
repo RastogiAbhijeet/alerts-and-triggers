ff8b87b713d1fb6559dd9c23d35e4fc5de1f5929
RastogiAbhijeet <abhijeetrastogi1997@gmail.com>
2021-09-12 07:34:16 +0530

    Add missing untracked

---
+++ sentry.client.config.js
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_ENABLED = process.env.SENTRY_ENABLED || process.env.NEXT_PUBLIC_SENTRY_ENABLED
+
+Sentry.init({
+  enabled: !!SENTRY_ENABLED && SENTRY_ENABLED !== "false" && SENTRY_ENABLED !== "no",
+  dsn: SENTRY_DSN || 'https://7ab434973dd24ca0b9f859a0873f3eb2@o971160.ingest.sentry.io/5955281',
+  tracesSampleRate: 1.0,
+});
