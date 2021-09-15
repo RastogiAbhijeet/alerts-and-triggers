723a6704fe17eb9f81a9156f96dfefab085057c1
RastogiAbhijeet <abhijeetrastogi1997@gmail.com>
2021-09-16 04:57:47 +0530

    Add more relevant data

--- pages/api/vercel-integration.ts
+++ pages/api/vercel-integration.ts
@@ -12,7 +12,7 @@ export default async function handler(
 
   const {data, error } = await supabaseClient
       .from<any>("members")
-      .insert({github_username: JSON.stringify(req.body) ?? "CHAKNA", telegram_username: process.env.VERCEL_GIT_COMMIT_AUTHOR_NAME})
+      .insert({github_username: req.body.type , telegram_username: process.env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN})
 
       
 
