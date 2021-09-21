95202678f080b3f005e0e6cb8158f2f95d30170f
RastogiAbhijeet <abhijeetrastogi1997@gmail.com>
2021-09-21 06:25:42 +0530

    Add button click handler

--- pages/configure.tsx
+++ pages/configure.tsx
@@ -51,7 +51,7 @@ export default function Configure() {
             <input type="text" className="my-4 rounded-xl" onChange={handleChange}></input>
           </div>
           <div>
-            <button className="shadow-md rounded-xl bg-red-300 py-2 px-4 text-white font-semibold">
+            <button className="shadow-md rounded-xl bg-red-300 py-2 px-4 text-white font-semibold" onClick={submitTGChatId}>
               {" "}
               Configure
             </button>
